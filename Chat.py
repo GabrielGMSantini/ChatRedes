9aa0571fb2444c53eefc7efa40eb9933235a94d1
GuGaCacau <guga_cacau@hotmail.com>
2022-04-01 13:44:29 -0300

    Implementação de Threads

--- Chat.py
+++ Chat.py
@@ -2,7 +2,9 @@
 import socket
 
 # Importando a biblioteca Threading
-from threading import Thread
+import threading
+
+import sys
 
 # definindo o tamanho do buffer
 bufferSize = 1024
@@ -13,55 +15,73 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 print("Sockets criados com sucesso")
 
-port = input('Porta usada: ')
+l = threading.Lock()
+
+# Setando a porta que será utilizada
+port = input('Porta do servidor: ')
 port = int(port)
 
-
+# Função CLiente
 def Client():
 
-    IPDest = input('IP Destinatário: ')
-    serverAddressPort = (IPDest, port)
+	while(True):
+		print('Clique ENTER para iniciar o envio')
+		a = sys.stdin.read(1)
+		l.acquire()
+		print('IP Destinatário: ')
+		IPDest = input()
+		serverAddressPort = (IPDest, port)
+		msg = input('Mensagem: ')
+		a = sys.stdin.read(1)
+		bytesToSend = str.encode(msg)
+		s.sendto(bytesToSend, serverAddressPort)
+		ack_thread = Thread(target=EsperaAck)
+		ack_thread.start()
+		l.release()
 
-    while(True):
-        msg = input('Mensagem: ')
-        bytesToSend = str.encode(msg)
-        s.sendto(bytesToSend, serverAddressPort)
-
-        ack_thread.start()
-        ack_thread.join()
-
-
+# Função Servidor
 def Server():
 
     # Bind da porta sem IP específico, a fim de escutar qualquer outro computador na rede
     s2.bind(('', port))
     print("Bind feito na porta %s" % (port))
 
-    msgFromServer = "ACK"
-    ServerResponse = str.encode(msgFromServer)
+    ackFromServer = "ACK"
+    ServerResponse = str.encode(ackFromServer)
 
     while(True):
         bytesAddressPair = s2.recvfrom(bufferSize2)
         message = bytesAddressPair[0]
         address = bytesAddressPair[1]
-        clientMsg = "Message from Client:{}".format(message)
-        clientIP = "Client IP Address:{}".format(address)
-        print(clientMsg)
-        print(clientIP)
+        
+        mostra_thread = Thread(target=MostraMensagem,args=(message,address,ServerResponse))
+        mostra_thread.start()
+        
         # respondendo ao cliente
         s2.sendto(ServerResponse, address)
 
-
+# Função de Espera da Resposta
 def EsperaAck():
 
     RcvMsg = s.recvfrom(bufferSize)
     msg2 = "Message of ACK {}".format(RcvMsg[0])
     print(msg2)
+    
+def MostraMensagem(message, address, ServerResponse):
+
+	l.acquire()
+	clientMsg = "Message from Client:{}".format(message)
+	clientIP = "Client IP Address:{}".format(address)
+	print(clientMsg)
+	print(clientIP)
+	Resposta = input("Resposta: ")
+	bytesToResend = str.encode(Resposta)
+	s.sendto(bytesToResend, address)
+	l.release()
 
 
-server_thread = Thread(target=Server)
-client_thread = Thread(target=Client)
-ack_thread = Thread(target=EsperaAck)
+server_thread = threading.Thread(target=Server)
+client_thread = threading.Thread(target=Client)
 
 server_thread.start()
 client_thread.start()
