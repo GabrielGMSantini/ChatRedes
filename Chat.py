309d134fce9de5730a7612e69db317c23d5eb372
GuGaCacau <guga_cacau@hotmail.com>
2022-04-04 01:31:19 -0300

    Finalização do README e do sistema

---
+++ Chat.py
@@ -0,0 +1,246 @@
+# Importando as bibliotecas necessárias
+import socket
+import threading
+import time
+import json
+
+
+# definindo funções de manipulação JSON
+def codificarMensagem(IP_origem, IP_destino, Porta_origem, Porta_destino, Timestamp, Mensagem):
+    x = {
+        "IP_origem": IP_origem,
+        "IP_destino": IP_destino,
+        "Porta_origem": Porta_origem,
+        "Porta_destino": Porta_destino,
+        "Timestamp da mensagem": Timestamp,
+        "Mensagem": Mensagem
+    }
+    return json.dumps(x)
+
+
+def codificarACK(IP_origem, IP_destino, Porta_origem, Porta_destino, Timestamporiginal, Timestampresposta, ACK):
+    x = {
+        "IP_origem": IP_origem,
+        "IP_destino": IP_destino,
+        "Porta_origem": Porta_origem,
+        "Porta_destino": Porta_destino,
+        "Timestamp da mensagem original": Timestamporiginal,
+        "Timestamp da mensagem de resposta": Timestampresposta,
+        "ACK": ACK
+    }
+    return json.dumps(x)
+
+
+def codificarResposta(IP_origem, IP_destino, Porta_origem, Porta_destino, Timestamporiginal, Timestampresposta, Mensagemoriginal, Mensagemresposta):
+    x = {
+        "IP_origem": IP_origem,
+        "IP_destino": IP_destino,
+        "Porta_origem": Porta_origem,
+        "Porta_destino": Porta_destino,
+        "Timestamp da mensagem original": Timestamporiginal,
+        "Timestamp da mensagem de resposta": Timestampresposta,
+        "Mensagem Original": Mensagemoriginal,
+        "Mensagem de resposta": Mensagemresposta
+    }
+    return json.dumps(x)
+
+
+# Criando uma função para printar o menu
+def printMenu():
+    print("\n-----------------Menu-----------------")
+    print("\nDigite [E] para Escrever uma mensagem")
+    print("Digite [R] para Responder uma mensagem")
+
+
+# definindo o tamanho dos buffers
+bufferSize = 1024
+bufferSize2 = 1024
+
+# Criando os objetos de socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+print("Sockets criados com sucesso")
+
+# Criando um Lock para controle
+l = threading.Lock()
+
+# Setando o IP do servidor
+ip = input('Seu IP: ')
+
+# Setando a porta do servidor
+port = input('Sua Porta: ')
+port = int(port)
+
+# Declaração da flag usada no menu para controle de thread
+global flagMenu
+flagMenu = 0
+
+
+# Função Cliente
+def Client():
+
+    # fazendo o bind do socket do cliente (necessário para conseguir recuperar a porta antes de ter enviado alguma coisa)
+    s.bind(('', port+1))
+    global flagMenu
+    flagMenu = 5
+    while(True):
+        # Print do Menu e input da opção de mensagem e resposta
+        if flagMenu != 0 and flagMenu != 2:
+            l.acquire()
+            time.sleep(2)
+            printMenu()
+            flagMenu = 0
+            l.release()
+            print('\n>> ', end='')
+        choice = input()
+        # Condicional para determinar o próximo print
+        if choice == 'E':
+            flagMenu = 1
+        elif choice == 'R':
+            flagMenu = 2
+        elif choice == "":
+            flagMenu = 0
+        else:
+            flagMenu = -1
+        if flagMenu == 1:
+            l.acquire()
+            print("\n------------Enviando Mensagem------------")
+            # Input do IP e da Porta do destinatário da mensagem
+            IPDest = input('\nIP Destinatário: ')
+            portaDest = input('\nPorta do Destinatário: ')
+            portaDest = int(portaDest)
+            # Armazenando a porta e o IP em um objeto
+            serverAddressPort = (IPDest, portaDest)
+            # Input da mensagem a ser enviada
+            msg = input('\nMensagem: ')
+            # JSON -> Byte
+            bytesToSend = str.encode(codificarMensagem(
+                ip, IPDest, s.getsockname()[1], portaDest, time.time(), msg))
+            # Envio da mensagem para o IP e a porta selecionadas
+            s.sendto(bytesToSend, serverAddressPort)
+            # Chamada da função de espera de ACK em outra thread
+            ack_thread = threading.Thread(target=EsperaAck)
+            ack_thread.start()
+            # Controle de print
+            flagMenu = 0
+            l.release()
+            # Condicional para opção inválida
+        elif flagMenu == -1:
+            print("\n\tOpção Inválida!!")
+
+
+# Função Servidor
+def Server():
+
+    # Bind da porta sem IP específico, a fim de escutar qualquer outro computador na rede
+    s2.bind(('', port))
+    s3.bind(('', port-1))
+    print("Bind feito na porta %s" % (port))
+    while(True):
+        # Servidor espera uma mensagem chegar para devolver outra de ACK
+        bytesAddressPair = s2.recvfrom(bufferSize2)
+        JSON = bytesAddressPair[0]
+        address = bytesAddressPair[1]
+        # JSON da mensagem é recebido como byte e transformado em JSON
+        decodedJSON = json.loads(JSON)
+        print("\nJSON da Mensagem:")
+        print(decodedJSON)
+        # Respondendo o ACK ao cliente
+        s2.sendto(str.encode(codificarACK(ip, decodedJSON["IP_origem"], decodedJSON["Porta_destino"],
+                  decodedJSON["Porta_origem"], decodedJSON["Timestamp da mensagem"], time.time(), True)), address)
+        # Chamada da função que mostra a mensagem e procede para resposta
+        mostra_thread = threading.Thread(
+            target=MostraMensagem, args=(decodedJSON["IP_origem"], decodedJSON["Porta_origem"], decodedJSON["Timestamp da mensagem"], decodedJSON["Mensagem"], address))
+        mostra_thread.start()
+
+
+# Função de Espera da Resposta
+def EsperaAck():
+    # Tempo de espera de 10 segundos para o ACK
+    s.settimeout(10)
+    try:
+        # Caso haja ACK, há print de seu JSON e há espera de resposta
+        RcvMsg = s.recvfrom(bufferSize)
+        s.settimeout(None)
+        JSONACK = RcvMsg[0]
+        decodedACK = json.loads(JSONACK)
+        print("\nMensagem Entregue")
+        print("ACK : " + str(decodedACK["ACK"]))
+        print("\nJSON do ACK:")
+        print(decodedACK)
+        EsperaResposta()
+    except Exception:
+        # Caso não haja ACK, é avisado para o cliente
+        print("\nMensagem não encontrou o alvo")
+        print("ACK : False")
+        printMenu()
+        print('\n>> ', end='')
+    s.settimeout(None)
+
+
+# Função que mostra a mensagem e produz uma resposta
+def MostraMensagem(IP_origem, Porta_origem, Timestamp, Mensagem, address):
+
+    message = Mensagem
+    global flagMenu
+    # Na tela é mostrada a mensagem e o IP do cliente
+    print("\n---------Mensagem Recebida---------")
+    clientMsg = "\nMensagem do Cliente: {}".format(message)
+    clientIP = "\nIP do Cliente: {}".format(IP_origem)
+    print(clientMsg)
+    print(clientIP)
+    # Repete-se o menu em relação à possibilidade de resposta
+    print("\nDigite [E] para Ignorar e Escrever uma mensagem")
+    print("Digite [R] para Responder a mensagem")
+    print("\n>> ", end='')
+    while True:
+        # Se escolhida a opção de resposta, o terminal pede a tecla ENTER
+        if flagMenu == 2:
+            l.acquire()
+            print("\n\nDigite [ENTER] para começar resposta")
+            print("\n>> ", end='')
+            # Input de resposta ao cliente
+            Resposta = input("\nResposta: ")
+            bytesToResend = str.encode(codificarResposta(ip, IP_origem, s2.getsockname()[
+                1], Porta_origem, Timestamp, time.time(), Mensagem, Resposta))
+            time.sleep(1)
+            # Envio de resposta e print de confirmação de envio para o servidor
+            s3.sendto(bytesToResend, address)
+            print("\n\nResposta Enviada com Sucesso")
+            printMenu()
+            print("\n>> ", end='')
+            l.release()
+            break
+
+
+# Função de espera de resposta
+def EsperaResposta():
+
+    # Função espera receber uma resposta do servidor
+    print("\nEsperando Resposta...")
+    RcvMsg = s.recvfrom(bufferSize)
+    JSONResposta = RcvMsg[0]
+    decodedResposta = json.loads(JSONResposta)
+    # Após receber a resposta e transformá-la em JSON, é printada para o cliente
+    msgOriginal = "\n\nMensagem Original: {}".format(
+        decodedResposta["Mensagem Original"])
+    msg2 = "\nMensagem de Resposta: {}".format(
+        decodedResposta["Mensagem de resposta"])
+    clientIP = "\nIP da Resposta: {}".format(decodedResposta["IP_origem"])
+    print(msgOriginal)
+    print(clientIP)
+    print(msg2)
+    print("\nJSON da Resposta:")
+    print(decodedResposta)
+    printMenu()
+    print("\n>> ", end='')
+
+
+# Criação das threads de servidor e cliente
+server_thread = threading.Thread(target=Server)
+client_thread = threading.Thread(target=Client)
+
+# Começo das threads
+server_thread.start()
+client_thread.start()
--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# ChatRedes
+<h1 align="center"> Chat entre Terminais usando Socket e Threads </h1>
+
+![Badge Finalizado](http://img.shields.io/static/v1?label=STATUS&message=FINALIZADO&color=RED&style=for-the-badge)
+
+<h2> Descrição do Projeto </h2>
+
+Projeto feito no laboratório 04 da disciplina de Redes de Computadores A, sobre o oitavo semestre do curso de Engenharia da Computação lecionado pelo professor Leandro Alonso Xastre, na Pontifícia Universidade Católica de Campinas, no ano de 2022.
+
+O projeto consiste em desenvolver um chat de terminal para linux onde um cliente e um servidor em computadores diferentes consigam mandar mensagens, receber confirmação de envio desta mensagem e enviar uma resposta para o mesmo IP do remetente da mensagem. A linguagem escolhida para a realização do mesmo foi o Python, com o uso de threads e sockets.
+
+## :hammer: Funcionalidades do projeto
+
+- `Envio de Mensagem`: Definindo-se um IP para a máquina e sua porta, é possível enviar mensagens utilizando soquetes.
+- `Receber ACK`: Para verificar se a mensagem chegou com sucesso no destinatário escolhido, é possível receber um sinal de ACK
+- `Envio de Resposta`: Com a confirmação de recebimento de mensagem, é possível que o destinatário da mensagem mande uma resposta para o servidor de quem a enviou
+
+## 📁 Acesso ao projeto
+
+O Acesso ao projeto é feito pelo Download do arquivo "Chat.py", sendo o Python 3 necessário para o rodar no terminal.
+
+## 🛠️ Abrir e rodar o projeto
+
+Ao executar o arquivo, primeiro é pedido que o usuário digite seu IP e a porta que o seu servidor usará, o que é importante para que outros usuários possam mandar mensagens nesse servidor. Desta forma, um menu irá aparecer com opções de envio e resposta. A opção de resposta apenas funcionará se houver alguma mensagem recebida pelo servidor.
+Escolhida a opção de envio, primeiro o sistema irá perguntar o IP e a porta do destinatário da mensagem e por fim, pedirá que o usuário escreva a mensagem, o que fará com que o servidor espere uma mensagem de ACK.
+Caso o ACK seja positivo, o sistema irá esperar uma resposta do destinatário, mas caso não seja, irá notificar o recebimento negativo do ACK e voltará ao menu.
+
+Para responder uma mensagem após recebê-la, o usuário deverá escolher a opção de resposta e logo após, teclar ENTER por medida de segurança a fim da limpeza de buffer, mandando assim a resposta que será notificada na tela de quem mandou a mensagem inicial.
+
+## Tecnologias Usadas
+
+-> Python
+
+<h2> Integrantes do Projeto </h2>
+
+Gabriel Gonçalves Mattos Santini
+Gustavo Campos Dias
+Gustavo Melo Cacau
